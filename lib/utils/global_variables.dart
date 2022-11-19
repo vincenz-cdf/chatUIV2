04f587ec544b2c617e8511d87ce6f451bae896c4
Vincent <85881442+vincenz-cdf@users.noreply.github.com>
2022-11-19 09:11:51 +0100

    search users

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:instalike/screens/add_post_screen.dart';
 import 'package:instalike/screens/feed_screen.dart';
+import 'package:instalike/screens/search_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
   FeedScreen(),
-  Text('search'),
+  SearchScreen(),
   AddPostScreen(),
   Text('notif'),
   Text('profile')
