68eed132a385a0a684ff11c6a3be44877064798e
Vincent <85881442+vincenz-cdf@users.noreply.github.com>
2023-01-28 11:38:31 +0100

    Affichage des données de profil

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instalike/screens/add_post_screen.dart';
 import 'package:instalike/screens/feed_screen.dart';
@@ -6,10 +7,12 @@ import 'package:instalike/screens/search_screen.dart';
 
 const webScreenSize = 600;
 
-const homeScreenItems = [
-  FeedScreen(),
-  SearchScreen(),
-  AddPostScreen(),
-  Text('notif'),
-  ProfileScreen()
+List<Widget> homeScreenItems = [
+  const FeedScreen(),
+  const SearchScreen(),
+  const AddPostScreen(),
+  const Text('notifications'),
+  ProfileScreen(
+    uid: FirebaseAuth.instance.currentUser!.uid,
+  ),
 ];
